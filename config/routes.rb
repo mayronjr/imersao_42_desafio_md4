e59ec6b8982b7c4db238f8bef38de5c3e4f031eb
Mayron Júnior <mayronjunior5@ufpi.edu.br>
2021-04-29 16:06:35 -0300

    Create Model and Controller for Post
    Controller for Posts contains
    - GET metods for all posts or one specific post and for a form to create new posts
    - POST metod for post

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "posts#index"
+
+  resources :posts
+  # get "/posts", to: "posts#index"
+  # get "/posts/:id", to: "posts#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
