c881ac0560b7f2ca86536d48027f7f4caf39675a
Mayron Júnior <mayronjunior5@ufpi.edu.br>
2021-04-29 16:53:49 -0300

    Add routes to editing and deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :author, :body)
